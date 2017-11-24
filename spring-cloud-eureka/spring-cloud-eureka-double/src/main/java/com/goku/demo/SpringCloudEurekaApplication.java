231990074354ef67e99edf454569eefb1a5147a1
nbfujx <nbfujx@qq.com>
2017-11-24 11:30:18 +0800

    新增spring-cloud-eureka项目

---
+++ spring-cloud-eureka/spring-cloud-eureka-double/src/main/java/com/goku/demo/SpringCloudEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.goku.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by nbfujx on 2017-11-24.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+    }
+}
---
+++ spring-cloud-eureka/spring-cloud-eureka-standalone/src/test/java/test/com/goku/demo/SpringCloudEurekaApplicationTests.java
@@ -0,0 +1,20 @@
+package test.com.goku.demo;
+
+
+import com.goku.demo.SpringCloudEurekaApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+/**
+ * Created by nbfujx on 2017-11-24.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=SpringCloudEurekaApplication.class)
+public class SpringCloudEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
