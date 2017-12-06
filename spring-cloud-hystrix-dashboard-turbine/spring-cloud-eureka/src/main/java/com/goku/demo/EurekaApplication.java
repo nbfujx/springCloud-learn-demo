8f2692f5ab9f58139fd05cbf8e2e7a9c8e785ba9
nbfujx <nbfujx@qq.com>
2017-12-06 08:55:32 +0800

    新增hystrix

---
+++ spring-cloud-hystrix-dashboard-turbine/spring-cloud-eureka/src/main/java/com/goku/demo/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.goku.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by nbfujx on 2017-11-24.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
