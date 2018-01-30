a780343ba5c3dfd907c6e072318b9e6c29608baa
nbfujx <nbfujx@qq.com>
2018-01-30 15:27:13 +0800

    mm

--- spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/goku/demo/ConfigClientApplication.java
+++ spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/goku/demo/ConfigClientApplication.java
@@ -1,7 +1,15 @@
 package com.goku.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by nbfujx on 2018-01-30.
  */
+@SpringBootApplication
 public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
 }
--- spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/goku/demo/controller/ExampleController.java
+++ spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/goku/demo/controller/ExampleController.java
@@ -2,6 +2,7 @@ package com.goku.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,13 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by nbfujx on 2017-11-20.
  */
 @RestController
+@RefreshScope
 public class ExampleController {
 
-    @Value("${word}")
-    private String word;
+    @Value("${goku.word}")
+    private String hello;
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String from() {
-        return this.word;
+        return this.hello;
     }
 }
